2e0461278715771cc7724b392dbb2378efde51bf
amnandan9 <amnandan9@gmail.com>
2025-07-15 00:00:00 +0000

    Refactor JavaScript modules

---
+++ src/file-1752537600000-0.js
@@ -0,0 +1,9 @@
+// src/file-1752537600000-0.js
+const greeting = "Hello, World!";
+console.log(greeting);
+
+function example() {
+  return "This is an example function";
+}
+
+module.exports = { example };
